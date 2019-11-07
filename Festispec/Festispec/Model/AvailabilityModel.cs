4d890b933073f30ac89d63fe7d056567bb74fd7c
MitchVerweerden <mverweer@avans.nl>
2019-11-07 21:15:07 +0100

    Beschikbaarheid POC aangemaakt Wellicht een plugin/api vinden voor calendar control

---
+++ Festispec/Festispec/Model/AvailabilityModel.cs
@@ -0,0 +1,29 @@
+﻿using Festispec.Model.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.Model
+{
+    public class AvailabilityModel
+    {
+        public int WeekNumber { get; set; }
+        public int DayNumber { get; set; }
+        public DateTime Date { get; set; }
+
+        public AvailabilityModel()
+        {
+
+        }
+
+        public AvailabilityModel(DateTime date)
+        {
+            this.Date = date;
+            this.WeekNumber = DateTimeExtension.WeekNumber(date);
+            this.DayNumber = (int)date.DayOfWeek+1;
+        }
+
+    }
+}
