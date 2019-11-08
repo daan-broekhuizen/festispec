b73dbac62c4886f14cd1ccc4973c453657d9f46d
RensAspersAvans <rca.asper@student.avans.nl>
2019-11-08 14:36:10 +0100

    initial push

---
+++ Festispec/FestiSpecDB/Klant.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Klant]
+(
+	[KvK_nummer] INT NOT NULL PRIMARY KEY, 
+    [Naam] VARCHAR(45) NOT NULL, 
+    [Postcode] VARCHAR(6) NOT NULL, 
+    [Huisnummer] VARCHAR(4) NOT NULL, 
+    [Email] VARCHAR(100) NOT NULL, 
+    [Website] VARCHAR(100) NULL, 
+    [Laatste_weiziging] DATETIME NOT NULL
+)
