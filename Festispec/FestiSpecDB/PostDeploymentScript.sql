9bad6bd6bbb5ce932e0082a2a42d0249eab60d45
RensAspersAvans <rca.asper@student.avans.nl>
2019-11-13 11:22:15 +0100

    seeds 4 lookup

---
+++ Festispec/FestiSpecDB/PostDeploymentScript.sql
@@ -0,0 +1,17 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+:r .\Seeds\Status_lookup_seed.sql
+:r .\Seeds\Rol_lookup_seed.sql
+:r .\Seeds\Vraagtype_lookup_seed.sql
+
---
+++ Festispec/FestiSpecDB/Seeds/Vraagtype_lookup_seed.sql
@@ -0,0 +1,16 @@
+﻿MERGE INTO dbo.Vraagtype_lookup AS Target
+USING (Values
+	('ov', 'Open vraag'),
+	('mv','Meerkeuze vraag'),
+	('av', 'Afbeelding vraag'),
+	('lv','Lijst vraag'),
+	('sv', 'Schaal vraag')
+
+) AS Source (Afkorting, Beschrijving)
+On 
+	Target.Afkorting = Source.Afkorting
+	
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT (Afkorting, Beschrijving)
+	VALUES (Afkorting, Beschrijving);
+
