80831bc58a03b772d7ac28bfe8584e7167ab5593
Bramcjb <bramcjb@outlook.com>
2019-11-13 12:39:26 +0100

    Update CustomerViewModel.cs

--- Festispec/Festispec/ViewModel/CustomerViewModels/CustomerViewModel.cs
+++ Festispec/Festispec/ViewModel/CustomerViewModels/CustomerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using FestiSpec.Domain;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,16 +11,131 @@ namespace Festispec.ViewModel
 {
     public class CustomerViewModel : ViewModelBase
     {
-        public string Name { get; set; }
-        public string PostalCode { get; set; }
-        public string HouseNumber { get; set; }
-        public string Addition { get; set; }
-        public string KvK { get; set; }
-        public string BranchNumber { get; set; }
-        public string Telephone { get; set; }
-        public string Email { get; set; }
-        public string Website { get; set; }
-        
+        private Klant _klant;
+        public string Name 
+        { 
+            get => _klant.Naam;
+            set
+            {
+                _klant.Naam = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        public string PostalCode
+        {
+            get => _klant.Postcode;
+            set
+            {
+                _klant.Postcode = value;
+                RaisePropertyChanged("PostalCode");
+            }
+        }
+
+        public string HouseNumber
+        {
+            get 
+            {
+                if (_klant.Huisnummer != 0)
+                    return Convert.ToString(_klant.Huisnummer);
+                else
+                    return "";
+            }
+            set
+            {
+                _klant.Huisnummer = Convert.ToInt32(value);
+                RaisePropertyChanged("HouseNumber");
+            }
+        }
+
+        public string Addition
+        {
+            get => _klant.Naam;
+            set
+            {
+                _klant.Naam = value;
+                RaisePropertyChanged("Addition");
+            }
+        }
+
+        public string KvK
+        {
+            get
+            {
+                if (_klant.KvK_nummer != 0)
+                    return Convert.ToString(_klant.KvK_nummer);
+                else
+                    return "";
+            }
+            set
+            {
+                _klant.KvK_nummer = Convert.ToInt32(value);
+                RaisePropertyChanged("KvK");
+            }
+        }
+
+        public string BranchNumber
+        {
+            get
+            {
+                if (_klant.KvK_nummer != 0)
+                    return Convert.ToString(_klant.KvK_nummer);
+                else
+                    return "";
+            }
+            set
+            {
+                _klant.KvK_nummer = Convert.ToInt32(value);
+                RaisePropertyChanged("BranchNumber");
+            }
+        }
+
+        public string Telephone
+        {
+            get
+            {
+                if (_klant.Huisnummer != 0)
+                    return Convert.ToString(_klant.Huisnummer);
+                else
+                    return "";
+            }
+            set
+            {
+                _klant.KvK_nummer = Convert.ToInt32(value);
+                RaisePropertyChanged("Telephone");
+            }
+        }
+
+        public string Email
+        {
+            get => _klant.Email;
+            set
+            {
+                _klant.Email = value;
+                RaisePropertyChanged("Email");
+            }
+        }
+
+        public string Website
+        {
+            get => _klant.Website;
+            set
+            {
+                _klant.Website = value;
+                RaisePropertyChanged("Website");
+            }
+        }
+
         public ObservableCollection<ContactPersonViewModel> Contacts { get; set; }
+
+        public CustomerViewModel(Klant klant)
+        {
+            _klant = klant;
+        }
+
+        public CustomerViewModel()
+        {
+            _klant = new Klant();
+        }
     }
 }
