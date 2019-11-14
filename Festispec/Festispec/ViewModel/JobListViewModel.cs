78dd823c5123b651e6219f0f297461019eff2c1f
timvandenberg <timvandenber9@gmail.com>
2019-11-14 14:30:34 +0100

    Inbetween

--- Festispec/Festispec/ViewModel/JobListViewModel.cs
+++ Festispec/Festispec/ViewModel/JobListViewModel.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 using FestiSpec.Domain.Repositories;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Festispec.ViewModel
 {
@@ -22,11 +25,149 @@ namespace Festispec.ViewModel
             }
         }
 
+        private List<JobViewModel> filteredJobs;
+
+        public List<JobViewModel> FilteredJobs
+        {
+            get => filteredJobs;
+            set
+            {
+                filteredJobs = value;
+                RaisePropertyChanged("FilteredJobs");
+            }
+        }
+
+        private JobViewModel _selectedJob;
+
+        public JobViewModel SelectedJob
+        {
+            get => _selectedJob;
+            set
+            {
+                _selectedJob = value;
+                RaisePropertyChanged("SelectedJob");
+            }
+        }
+
+        private ComboBoxItem _selectedBox;
+        public ComboBoxItem SelectedBox
+        {
+            get
+            {
+                return _selectedBox;
+            }
+            set
+            {
+                _selectedBox = value;
+                SortJobs();
+            }
+
+        }
+
+        public string FilterJob { get; set; }
+
         private JobRepository JobRepository { get; set; }
+
+        public ICommand SearchJob { get; set; }
         public JobListViewModel()
         {
             JobRepository = new JobRepository();
             Jobs = JobRepository.GetOpdrachten().Select(c => new JobViewModel(c)).ToList();
+            FilteredJobs = Jobs;
+            SearchJob = new RelayCommand(FilterJobs);
+            FilterJob = "";
+        }
+
+        public void FilterJobs()
+        {
+            if (SelectedBox != null)
+            {
+                switch (SelectedBox.Content)
+                {
+                    case "A - Z":
+                        FilteredJobs = GetFilteredOpdrachtenASC();
+                        break;
+                    case "Z - A":
+                        FilteredJobs = GetFilteredOpdrachtenDESC();
+                        break;
+                }
+            }
+            else
+            {
+                FilteredJobs = GetFilteredOpdrachten();
+
+            }
+        }
+
+        public void SortJobs()
+        {
+            if (FilterJob != null)
+            {
+                if (SelectedBox != null)
+                {
+                    switch (SelectedBox.Content)
+                    {
+                        case "A - Z":
+                            FilteredJobs = GetFilteredOpdrachtenASC();
+                            break;
+                        case "Z - A":
+                            FilteredJobs = GetFilteredOpdrachtenDESC();
+                            break;
+                    }
+                }
+                else
+                {
+                    FilteredJobs = GetFilteredOpdrachten();
+
+
+                }
+            }
+            else
+            {
+                if (SelectedBox != null)
+                {
+                    switch (SelectedBox.Content)
+                    {
+                        case "A - Z":
+                            FilteredJobs = GetOpdrachtenASC();
+                            break;
+                        case "Z - A":
+                            FilteredJobs = GetOpdrachtenDESC();
+                            break;
+                    }
+                }
+                else
+                {
+                    FilteredJobs = Jobs;
+
+                }
+            }
+        }
+
+        public List<JobViewModel> GetFilteredOpdrachten()
+        {
+            FilteredJobs = Jobs.Where(e => e.JobName.Contains(FilterJob)).ToList();
+            return FilteredJobs;
+        }
+        public List<JobViewModel> GetFilteredOpdrachtenASC()
+        {
+            FilteredJobs = Jobs.Where(e => e.JobName.Contains(FilterJob)).OrderBy(e => e.JobName).ToList();
+            return FilteredJobs;
+        }
+        public List<JobViewModel> GetFilteredOpdrachtenDESC()
+        {
+            FilteredJobs = Jobs.Where(e => e.JobName.Contains(FilterJob)).OrderByDescending(e => e.JobName).ToList();
+            return FilteredJobs;
+        }
+        public List<JobViewModel> GetOpdrachtenASC()
+        {
+            FilteredJobs = Jobs.OrderBy(e => e.JobName).ToList();
+            return FilteredJobs;
+        }
+        public List<JobViewModel> GetOpdrachtenDESC()
+        {
+            FilteredJobs = Jobs.OrderByDescending(e => e.JobName).ToList();
+            return FilteredJobs;
         }
     }
 }
