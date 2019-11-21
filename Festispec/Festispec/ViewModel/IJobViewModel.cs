54c24a8a0c41b82da4c2cea576f538393c662aa4
Bramcjb <bramcjb@outlook.com>
2019-11-21 21:18:12 +0100

    start views

---
+++ Festispec/Festispec/ViewModel/IJobViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.ViewModel
+{
+    public interface IJobViewModel
+    {
+        int Id { get; set; }
+        CustomerViewModel CustomerVM { get; set; }
+    }
+}
