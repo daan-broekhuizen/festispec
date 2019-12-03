de341e4407252a83e656f93cf47a8280576d9f27
Wesley van Osch <wesley@opjeklompen.nl>
2019-12-03 23:38:28 +0100

    Update ViewModelLocator.cs

--- Festispec/Festispec/ViewModel/ViewModelLocator.cs
+++ Festispec/Festispec/ViewModel/ViewModelLocator.cs
@@ -53,6 +53,7 @@ namespace Festispec.ViewModel
             SimpleIoc.Default.Register<UserRepository>();
             SimpleIoc.Default.Register<QuotationRepository>();
             SimpleIoc.Default.Register<TemplateRepository>();
+            SimpleIoc.Default.Register<RapportageRepository>();
         }
         //Register singeltonviews here
         private static void RegisterViewModels()
@@ -124,6 +125,7 @@ namespace Festispec.ViewModel
         public ApplicationViewModel Application => ServiceLocator.Current.GetInstance<ApplicationViewModel>();
         public LoginViewModel Login => new LoginViewModel(ServiceLocator.Current.GetInstance<NavigationService>(), UserRepo);
         public RapportageViewModel Rapportage => new RapportageViewModel(ServiceLocator.Current.GetInstance<NavigationService>(), RapportageRepo);
+        public CreateInspectionFormViewModel CreateInspectionForm => new CreateInspectionFormViewModel(ServiceLocator.Current.GetInstance<NavigationService>());
         #endregion
 
         #region CustomerVM's
