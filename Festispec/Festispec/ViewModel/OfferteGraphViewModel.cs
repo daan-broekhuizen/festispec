80fba5fc044375f8f1c190109cba3cce91aab650
timvandenberg <timvandenber9@gmail.com>
2019-12-03 11:43:08 +0100

    Inbetween

--- Festispec/Festispec/ViewModel/OfferteGraphViewModel.cs
+++ Festispec/Festispec/ViewModel/OfferteGraphViewModel.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveCharts;
 
 namespace Festispec.ViewModel
 {
     public class OfferteGraphViewModel
     {
+        private GraphRepository _repo;
 
+        public SeriesCollection OffertesCollection { get; set; }
+
+        public OfferteGraphViewModel()
+        {
+            _repo = new GraphRepository();
+
+            OffertesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Afgewezen",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(_repo.GetOffertesAmountByStatus)}
+            }
+        }
     }
 }
