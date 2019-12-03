66ce2245b2e06ea130548d5fbe91119a487f561f
timvandenberg <timvandenber9@gmail.com>
2019-12-03 11:57:58 +0100

    Inbetween

--- Festispec/Festispec/Model/Repositories/GraphRepository.cs
+++ Festispec/Festispec/Model/Repositories/GraphRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Festispec.Enumerable
+namespace Festispec.Model.Repositories
 {
-    class EnumOfferteStatus
+    class GraphRepository
     {
     }
 }
