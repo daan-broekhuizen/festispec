5e89c8c811e94b48bad803b5581c9d04fb7e05ba
Daan <drpm.broekhuizen@student.avans.nl>
2019-12-04 21:15:29 +0100

    inbetween

---
+++ Festispec/Festispec.WebApplication/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Festispec.WebApplication.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
