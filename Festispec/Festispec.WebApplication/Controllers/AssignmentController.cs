32fb59b1376b493b84760999eb1e7ca985aebea9
Daan <drpm.broekhuizen@student.avans.nl>
2019-12-06 02:24:35 +0100

    search added

---
+++ Festispec/Festispec.WebApplication/Controllers/AssignmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Festispec.WebApplication.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        // GET: Assignment
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
