bd370313728228d29c4e64821111beba2ea33f64
Mike  Damen <mjm.damen@student.avans.nl>
2019-12-12 16:09:11 +0100

    First commit

---
+++ Festispec/Festispec/ViewModel/UserRightsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Festispec.Model;
+using Festispec.Service;
+using Festispec.ViewModel.CustomerViewModels;
+using FestiSpec.Domain;
+using FestiSpec.Domain.Repositories;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+namespace Festispec.ViewModel
+{
+    public class UserRightsViewModel : ViewModelBase
+    {
+        public ObservableCollection<AccountViewModel> Accounts;
+        private List<AccountViewModel> filteredAccounts;
+
+        public List<AccountViewModel> FilteredAccounts
+        {
+            get => filteredAccounts;
+            set
+            {
+                filteredAccounts = value;
+                RaisePropertyChanged("FilteredAccounts");
+            }
+        }
+
+        private AccountViewModel selectedAccount;
+        public AccountViewModel SelectedAccount
+        {
+            get => selectedAccount;
+            set
+            {
+                selectedAccount = value;
+                RaisePropertyChanged("SelectedAccount");
+                //ShowCustomerInfo();
+            }
+        }
+    }
+}
