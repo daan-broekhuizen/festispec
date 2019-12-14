1890cd4f9975a2f47bf28cd30363ba5c1601056f
MitchVerweerden <mverweer@avans.nl>
2019-12-14 11:10:43 +0100

    Sqlite toegevoegd

---
+++ Festispec/Festispec/Model/Repositories/OfflineInspectionsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.Model.Repositories
+{
+    public class OfflineInspectionsRepository
+    {
+        public List<Klant> GetOfflineCustomers()
+        {
+            using(IDbConnection cnn = new SQLiteConnection())
+            {
+
+            }
+        }
+    }
+
+    private static string LoadConnectionString(string id = "Default")
+    {
+        return ConfigurationManager.ConnectionStrings[].ConnectionString
+    }
+}
---
+++ Festispec/Festispec/ViewModel/OfflineViewModel/OfflineDashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Festispec.Service;
+
+namespace Festispec.ViewModel.OfflineViewModel
+{
+    public class OfflineDashboardViewModel : NavigatableViewModel
+    {
+        public ICommand ShowJobsCommand { get; set; }
+        public OfflineDashboardViewModel(NavigationService service) : base(service)
+        {
+
+        }
+    }
+}
