e0fca138d3dd403ee3f937129c92fe9bc4848973
Bramcjb <bramcjb@outlook.com>
2019-12-15 17:38:06 +0100

    Date validation check

--- Festispec/Festispec.WebApplication/Models/InspectionFormViewModel.cs
+++ Festispec/Festispec.WebApplication/Models/InspectionFormViewModel.cs
@@ -11,6 +11,10 @@ namespace Festispec.WebApplication.Models
         public List<QuestionViewModel> Questions { get; set; }
         public string LogoString => Convert.ToBase64String(Inspectionform.Opdracht.Klant.Klant_logo);
         public int CompletedQuestions { get; set; }
+        public bool IsEditable
+        {
+            get => DateTime.Now.Date == Inspectionform.DatumInspectie;
+        }
         
     }
 }
