a094839c5915157ea559eab20aa717ca4a894bf7
MitchVerweerden <mverweer@avans.nl>
2019-12-26 10:17:50 +0100

    Framework toegevoegd

---
+++ Festispec/Festispec.WebApplication/Controllers/Planning/PlanningAPIController.cs
@@ -0,0 +1,28 @@
+﻿using Festispec.WebApplication.Models;
+using Festispec.WebApplication.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Festispec.WebApplication.Controllers.Planning
+{
+    public class PlanningAPIController : ApiController
+    {
+        private FestiSpecContext db = new FestiSpecContext();
+
+        public IEnumerable<WebAPIEvent> Get()
+        {
+            return db.Inspectieformulier
+                    .ToList()
+                    .Select(e => (WebAPIEvent)e);
+        }
+
+        public WebAPIEvent Get(int id)
+        {
+            return (WebAPIEvent)db.Inspectieformulier.Find(id);
+        }
+    }
+}
---
+++ Festispec/Festispec.WebApplication/Models/DTO/WebAPIEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Festispec.WebApplication.Models.DTO
+{
+    public class WebAPIEvent
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public string start_date { get; set; }
+        public string end_date { get; set; }
+
+        public static explicit operator WebAPIEvent(Inspectieformulier inspectie)
+        {
+            if (inspectie.Datum_inspectie != null)
+            {
+                return new WebAPIEvent
+                {
+                    id = (int)inspectie.OpdrachtID,
+                    text = inspectie.InspectieFormulierTitel,
+                    start_date = inspectie.Datum_inspectie.GetValueOrDefault().ToString("yyyy-MM-dd HH:mm"),
+                    end_date = inspectie.Datum_inspectie.GetValueOrDefault().ToString("yyyy-MM-dd HH:mm")
+                };
+            }
+            else
+                return null;
+        }
+
+        
+    }
+}
