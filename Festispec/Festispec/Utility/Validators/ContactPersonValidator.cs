c92bb59df9f5c29edd409560c2190f363dd4a89e
Bramcjb <bramcjb@outlook.com>
2019-12-30 09:27:13 +0100

    max length fix

--- Festispec/Festispec/Utility/Validators/ContactPersonValidator.cs
+++ Festispec/Festispec/Utility/Validators/ContactPersonValidator.cs
@@ -14,7 +14,13 @@ namespace Festispec.Validators
         public ContactPersonValidator()
         {
             RuleFor(x => x.FirstName).NotEmpty().WithMessage("Voer een voornaam in.");
+            RuleFor(x => x.FirstName).MaximumLength(30).WithMessage("Voornaam te lang (max 30).");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Voer een achternaam in.");
+            RuleFor(x => x.LastName).MaximumLength(30).WithMessage("Achternaam te lang (max 30).");
+            RuleFor(x => x.Infix).MaximumLength(15).WithMessage("Tussenvoegsel te lang (max 15).");
+            RuleFor(x => x.Role).MaximumLength(30).WithMessage("Rol te lang (max 30).");
             RuleFor(x => x.Email).NotEmpty().WithMessage("Voer een email adres in");
+            RuleFor(x => x.Email).MaximumLength(130).WithMessage("Email te lang (max 130).");
             RuleFor(x => x.Telephone).Must(IsValidTelephone).WithMessage("Voer een geldig telefoonnummer in (0612345678).");
         }
 
--- Festispec/Festispec/Utility/Validators/CustomerValidator.cs
+++ Festispec/Festispec/Utility/Validators/CustomerValidator.cs
@@ -16,23 +16,24 @@ namespace Festispec.Validators
         public CustomerValidator()
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("Voer een naam in.");
+            RuleFor(x => x.Name).MaximumLength(45).WithMessage("Klantnaam te lang (max 45).");
             RuleFor(x => x.KvK).NotEmpty().WithMessage("Voer een KvK nummer in.");
             RuleFor(x => x.KvK).Must(IsNumericalSequence).Length(8).WithMessage("Voer een geldig KvK nummer in (8 cijfers).");
             RuleFor(x => x.Branchnumber).Must(IsNumericalSequence).Length(12).WithMessage("Voer een geldig vestigingsnummer in (12 cijfers).");
             RuleFor(x => x.Streetname).NotEmpty().WithMessage("Voer een straatnaam in.");
+            RuleFor(x => x.Streetname).MaximumLength(50).WithMessage("Straatnaam te lang (max 50).");
             RuleFor(x => x.HouseNumber).NotEmpty().WithMessage("Voer een huisnummer in.");
-            RuleFor(x => x.HouseNumber).Must(IsValidHouseNumber).WithMessage("Huisnummer + toevoeging mag max. 4 lang zijn");
+            RuleFor(x => x.HouseNumber).MaximumLength(4).WithMessage("Huisnummer + toevoeging mag max. 4 lang zijn");
             RuleFor(x => x.City).NotEmpty().WithMessage("Voer een plaastnaam in.");
+            RuleFor(x => x.City).MaximumLength(50).WithMessage("Plaatsnaam te lang (max 50).");
             RuleFor(x => x.PostalCode).Must(IsValidPostalCode).WithMessage("Address is ongeldig, zie postcode.");
             RuleFor(x => x.Email).NotEmpty().WithMessage("Voer een email adres in.");
+            RuleFor(x => x.Email).MaximumLength(130).WithMessage("Email te lang (max 130).");
+            RuleFor(x => x.Website).MaximumLength(100).WithMessage("Website te lang (max 100).");
             RuleFor(x => x.Telephone).Length(10).WithMessage("Voer een geldig telefoonnummer in (10 cijfers).");
             RuleFor(x => x.Telephone).Must(IsNumericalSequence).WithMessage("Voer een geldig telefoonnummer in (0612345678).");
         }
-        private bool IsValidHouseNumber(string arg)
-        {
-            if (arg == null) return true;
-            return arg.Length <= 4;
-        }
+
 
         private bool IsNumericalSequence(string arg)
         {
